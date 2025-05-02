54c5d0706ff49968368465cc7fdadd815f0367c0
Mani-420 <tahirabdulrehman46@gmail.com>
2025-05-03 02:21:55 +0500

    Auth

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -9,7 +9,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
       req.cookies.accessToken ||
       req.header('Authorization')?.replace('Bearer ', '');
 
-    console.log('Token received:', token);
+    // console.log('Token received:', token);
 
     if (!token) {
       throw new ApiError(401, 'Unauthorized access');
