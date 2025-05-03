4448a0adb5b215abba3d090f0186fa8905af384f
Mani-420 <tahirabdulrehman46@gmail.com>
2025-05-04 01:03:32 +0500

    Main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
+        {/* <Footer /> */}
       </BrowserRouter>
     </>
   );
