d65b3089289bf8e9f9db661a2b9abf1cb876c89a
Mani-420 <tahirabdulrehman46@gmail.com>
2025-05-05 20:27:50 +0500

    Main

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 // CORS Middleware
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 const corsOptions = {
   origin: 'http://localhost:5173',
   credentials: true,
