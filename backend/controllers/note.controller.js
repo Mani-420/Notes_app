dba4abd57ef9b040fbf64e4c7ec1057083d35c89
Mani-420 <tahirabdulrehman46@gmail.com>
2025-05-05 18:11:24 +0500

    Note Controller

--- backend/controllers/note.controller.js
+++ backend/controllers/note.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 import { ApiError } from '../utils/ApiError.js';
-import { Note } from '../models/note.model.js';
+import { Notes } from '../models/notes.model.js';
 
 const createNote = asyncHandler(async (req, res) => {
   const { title, content } = req.body;
