5e0b524d3420512dc3fea0b4285cc6f80d07b621
Mani-420 <tahirabdulrehman46@gmail.com>
2025-05-08 00:04:02 +0500

    Main

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,13 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 // CORS Middleware
-// app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
-const corsOptions = {
-  origin: 'http://localhost:5173',
-  credentials: true,
-  optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // or your frontend URL
+    credentials: true,
+    optionsSuccessStatus: 200
+  })
+);
 
 // Other Middlewares
 app.use(express.json({ limit: '16kb' }));
