1440e6ced10cdaf4ff413de99c865b97f9a778bf
Mani-420 <tahirabdulrehman46@gmail.com>
2025-05-08 15:23:23 +0500

    Notes Route

--- backend/routes/note.routes.js
+++ backend/routes/note.routes.js
@@ -3,6 +3,7 @@ import {
   createNote,
   editNote,
   deleteNote,
+  singleNote,
   getAllNotes
 } from '../controllers/note.controller.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
@@ -12,6 +13,7 @@ const router = Router();
 // Secure routes with JWT middleware
 router.route('/create-note').post(verifyJWT, createNote);
 router.route('/').get(verifyJWT, getAllNotes);
+router.route('/:id').get(verifyJWT, singleNote);
 router.route('/edit/:id').put(verifyJWT, editNote);
 router.route('/delete/:id').delete(verifyJWT, deleteNote);
 
