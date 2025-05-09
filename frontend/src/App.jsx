17b897df80ce8739fbd7b658177df48f5bb9ca5c
Mani-420 <tahirabdulrehman46@gmail.com>
2025-05-09 22:00:54 +0500

    Main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home/Home.jsx';
 import Login from './pages/Login/Login.jsx';
 import Signup from './pages/Signup/Signup.jsx';
 import Navbar from './components/Navbar.jsx';
+import CreateNote from './pages/AddNotes/CreateNote.jsx';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signup />} />
+        <Route path="/create-note" element={<CreateNote />} />
       </Routes>
     </BrowserRouter>
   );
