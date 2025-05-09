c73df9b86f44374ea7c7760112a5533f44edc1d4
Mani-420 <tahirabdulrehman46@gmail.com>
2025-05-10 02:09:32 +0500

    Main

--- backend/app.js
+++ backend/app.js
@@ -7,15 +7,15 @@ const app = express();
 // CORS Middleware
 app.use(
   cors({
-    origin: 'http://localhost:5173', // or your frontend URL
+    origin: '*', // or your frontend URL
     credentials: true,
     optionsSuccessStatus: 200
   })
 );
 
 // Other Middlewares
-app.use(express.json({ limit: '16kb' }));
-app.use(express.urlencoded({ limit: '16kb', extended: true }));
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static('public'));
 app.use(cookieParser());
 
@@ -24,7 +24,7 @@ import userRoute from './routes/user.routes.js';
 import noteRoute from './routes/note.routes.js';
 
 // Routes Declaration
-app.use('/api/v1/users', userRoute);
-app.use('/api/v1/notes', noteRoute);
+app.use('/api/users', userRoute);
+app.use('/api/notes', noteRoute);
 
 export { app };
