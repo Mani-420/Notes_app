9653c341fcbfdee35192c8ab042934214e953dd8
Mani-420 <tahirabdulrehman46@gmail.com>
2025-05-09 21:29:52 +0500

    Helper Func

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -11,3 +11,9 @@ export const getInitials = (name) => {
 
   return initials.toUpperCase();
 };
+
+export const validateEmail = (email) => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  return regex.test(email);
+};
