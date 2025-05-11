02f745d77997e58188cbf542667ff5e5d3474096
Mani-420 <tahirabdulrehman46@gmail.com>
2025-05-11 22:21:54 +0500

    Main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import Login from './pages/Login/Login.jsx';
 import Signup from './pages/Signup/Signup.jsx';
 import Navbar from './components/Navbar.jsx';
 import CreateNote from './pages/AddNotes/CreateNote.jsx';
+import EditNote from './pages/EditNotes/EditNote.jsx';
+import ViewNote from './pages/ViewNote/ViewNote.jsx';
 
 const App = () => {
   return (
@@ -15,6 +17,8 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signup />} />
         <Route path="/create-note" element={<CreateNote />} />
+        <Route path="/notes/:id" element={<ViewNote />} />
+        <Route path="/notes/edit/:id" element={<EditNote />} />
       </Routes>
     </BrowserRouter>
   );
