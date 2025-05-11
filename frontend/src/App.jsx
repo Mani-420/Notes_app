ebcfff0b00eda6b0fb910d5ba82c43a62e25ea10
Mani-420 <tahirabdulrehman46@gmail.com>
2025-05-11 16:15:41 +0500

    Main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home.jsx';
 import Login from './pages/Login/Login.jsx';
